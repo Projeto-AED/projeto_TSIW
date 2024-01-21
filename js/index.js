aad983b2427b3f13aed7f8333345926b7c57238f
miguelfp17 <miguelfrancapereira@hotmail.com>
2024-01-21 01:13:50 +0000

    página individual de filme

--- js/index.js
+++ js/index.js
@@ -52,5 +52,17 @@ galleryImages.forEach(image => {
 lightbox.addEventListener('click', () => {
     lightbox.style.display = 'none';
 });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-AOS.init();
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
